14bf5764f037b661ffa8a250657f7092da575813
COSTA Dev <guilhermeoliveira.tech@outlook.com.br>
2025-11-28 21:00:53 -0300

    adicionadas os metodos  de login,get e delete

--- src/main/java/com/COSTADev/usuario/business/dto/UsuarioDTO.java
+++ src/main/java/com/COSTADev/usuario/business/dto/UsuarioDTO.java
@@ -1,5 +1,7 @@
 package com.COSTADev.usuario.business.dto;
 
+import com.COSTADev.usuario.infrasctruture.entity.Enderecos;
+import com.COSTADev.usuario.infrasctruture.entity.Telefone;
 import lombok.*;
 
 import java.util.List;
@@ -11,7 +13,10 @@ import java.util.List;
 @Builder
 public class UsuarioDTO {
 
+    private String nome;
     private String email;
     private String senha;
 
+    private List<EnderecosDTO> enderecosDTOS;
+    private List<TelefoneDTO> telefoneDTOS;
 }
